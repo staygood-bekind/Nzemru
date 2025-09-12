423b156f037ba670138513891554320207f955e2
staygood-bekind <staygoodbekind@gmail.com>
2025-09-12 11:57:16 +0300

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,21 +1,93 @@
 const lyricsData = {
-  song1: {
-    title: { tig: "ኣብዚ ዘብጽሕካኒ", eng: "Abzi Zebxahkani" },
+song1: {
+    title: { tig: "ናይ ሶንግ 1", eng: "Song 1" },
     author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
     album: { tig: "ንስል", eng: "Nsil" },
     genre: { tig: "ምስጋና", eng: "Praise" },
-    date: "2023-02-23",
-    tig: `1ኣብዚ ዘብጽሕካኒ ንስኻ ኢኻ አብዚ ...`,
-    eng: `abzi zebxaHkani nsKa iKa ...`
-  },
-  song2: {
-    title: { tig: "ብግብርኻ ተደኒቀ", eng: "By Your Work You Were Saved" },
+    date: "2023-03-01",
+    tig: `1ናይ ሶንግ 1 ትርጉም ...`,
+    eng: `1 Song 1 lyrics ...`
+},
+song2: {
+    title: { tig: "ናይ ሶንግ 2", eng: "Song 2" },
     author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
     album: { tig: "ንስል", eng: "Nsil" },
     genre: { tig: "ምስጋና", eng: "Praise" },
-    date: "2023-02-23",
-    tig: `2ብግብርኻ ተደኒቀ ...`,
-    eng: `By your work you were saved ...`
-  },
- //add here
+    date: "2023-03-02",
+    tig: `2ናይ ሶንግ 2 ትርጉም ...`,
+    eng: `2 Song 2 lyrics ...`
+},
+song3: {
+    title: { tig: "ናይ ሶንግ 3", eng: "Song 3" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-03",
+    tig: `3ናይ ሶንግ 3 ትርጉም ...`,
+    eng: `3 Song 3 lyrics ...`
+},
+song4: {
+    title: { tig: "ናይ ሶንግ 4", eng: "Song 4" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-04",
+    tig: `4ናይ ሶንግ 4 ትርጉም ...`,
+    eng: `4 Song 4 lyrics ...`
+},
+song5: {
+    title: { tig: "ናይ ሶንግ 5", eng: "Song 5" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-05",
+    tig: `5ናይ ሶንግ 5 ትርጉም ...`,
+    eng: `5 Song 5 lyrics ...`
+},
+song6: {
+    title: { tig: "ናይ ሶንግ 6", eng: "Song 6" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-06",
+    tig: `6ናይ ሶንግ 6 ትርጉም ...`,
+    eng: `6 Song 6 lyrics ...`
+},
+song7: {
+    title: { tig: "ናይ ሶንግ 7", eng: "Song 7" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-07",
+    tig: `7ናይ ሶንግ 7 ትርጉም ...`,
+    eng: `7 Song 7 lyrics ...`
+},
+song8: {
+    title: { tig: "ናይ ሶንግ 8", eng: "Song 8" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-08",
+    tig: `8ናይ ሶንግ 8 ትርጉም ...`,
+    eng: `8 Song 8 lyrics ...`
+},
+song9: {
+    title: { tig: "ናይ ሶንግ 9", eng: "Song 9" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-09",
+    tig: `9ናይ ሶንግ 9 ትርጉም ...`,
+    eng: `9 Song 9 lyrics ...`
+},
+song10: {
+    title: { tig: "ናይ ሶንግ 10", eng: "Song 10" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
+    date: "2023-03-10",
+    tig: `10ናይ ሶንግ 10 ትርጉም ...`,
+    eng: `10 Song 10 lyrics ...`
+},
+// … continue incrementally up to song64
 };
