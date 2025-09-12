9a5c620f0c03ffad9db435431c63b3986e343151
staygood-bekind <staygoodbekind@gmail.com>
2025-09-12 11:22:10 +0300

    Update lyrics.js

--- lyrics.js
+++ lyrics.js
@@ -1,47 +1,30 @@
 const lyricsData = {
   song1: {
     title: { tig: "ኣብዚ ዘብጽሕካኒ", eng: "Abzi Zebxahkani" },
-    author: "ሴም ፡ ኣድያም",
-    album: "ንስል",
-    genre: "ምስጋና",
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
     date: "2023-02-23",
     tig: `1ኣብዚ ዘብጽሕካኒ ንስኻ ኢኻ አብዚ ...`,
     eng: `abzi zebxaHkani nsKa iKa ...`
   },
   song2: {
     title: { tig: "ብግብርኻ ተደኒቀ", eng: "By Your Work You Were Saved" },
-    author: "ሴም ፡ ኣድያም",
-    album: "ንስል",
-    genre: "ምስጋና",
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
     date: "2023-02-23",
     tig: `2ብግብርኻ ተደኒቀ ...`,
     eng: `By your work you were saved ...`
   },
-  song3: {
-    title: { tig: "ብግብርኻ ተደኒቀ", eng: "By Your Work You Were Saved" },
-    author: "ሴም ፡ ኣድያም",
-    album: "ንስል",
-    genre: "ምስጋና",
-    date: "2023-02-23",
-    tig: `3ብግብርኻ ተደኒቀ ...`,
-    eng: `By your work you were saved ...`
-  },
   song4: {
-    title: { tig: "ብግብርኻ ተደኒቀ", eng: "bgbrka tedeniqe" },
-    author: "ሴም ፡ ኣድያም",
-    album: "ንስል",
-    genre: "ምስጋና",
+    title: { tig: "ብግብርኻ ተደኒቀ", eng: "Bgbrka Tedeniqe" },
+    author: { tig: "ሴም ፡ ኣድያም", eng: "Sem Adiam" },
+    album: { tig: "ንስል", eng: "Nsil" },
+    genre: { tig: "ምስጋና", eng: "Praise" },
     date: "2023-02-23",
     tig: `1) ኣየሕፍረንን ትህላውነትካ ...`,
     eng: `1) Ayyeḥfrenn tihlawnetekha ...`
-  },
-  song5: {
-    title: { tig: "ብግብርኻ ተደኒቀ", eng: "By Your Work You Were Saved" },
-    author: "ሴም ፡ ኣድያም",
-    album: "ንስል",
-    genre: "ምስጋና",
-    date: "2023-02-23",
-    tig: `6ብግብርኻ ተደኒቀ ...`,
-    eng: `By your work you were saved ...`
   }
+  // add more songs here
 };
